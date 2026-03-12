c41bb5ed7836550d33f39ff809ad8339463b2a14
Sachin Kumar Thakur <sachinkumarthakur878@gmail.com>
2026-03-12 18:13:25 +0530

    fix cors for vercel preview

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,26 @@ const allowedOrigins = [
 
 app.use(express.json());
 
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true
+// }));
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+  origin: function(origin, callback) {
+
+    if(!origin) return callback(null,true)
+
+    if(
+      origin.includes("localhost") ||
+      origin.includes("vercel.app")
+    ){
+      return callback(null,true)
+    }
+
+    return callback(new Error("Not allowed by CORS"))
+  },
+  credentials:true
+}))
 
 app.use("/users", users);
 app.use("/books", books);
